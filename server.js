694f2f81ef5e6cc7f297d57fb4c1b97e2b7539c9
Marklar <dmitri.bilyk@gmail.com>
2017-12-29 01:36:38 -0800

    template core is setup

--- server.js
+++ server.js
@@ -8,9 +8,16 @@ app.set("views", path.join(__dirname,"/views"))
 app.set("view engine","pug")
 app.use(sass({
   src: path.join(__dirname,"sass"),
-  dest: path.join(__dirname,"public"),
-  outputStyle: "compressed",
+  dest: path.join(__dirname,"public/stylesheets"),
+  outputStyle: "compressed"
 
 }))
 app.use(express.static("/public"))
+
+app.get("/",(req,res)=>{
+  
+   
+  res.render(path.join(__dirname,"views/rooms.pug"));
+})
+
 server.listen(3000,"localhost",()=> console.log("server started"))
