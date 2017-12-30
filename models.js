f004521d18d1c588c634d491cb378b30360fad4b
Marklar <dmitri.bilyk@gmail.com>
2017-12-30 03:49:14 -0800

    model up and running with mysql

--- models.js
+++ models.js
@@ -1,1 +1,20 @@
-const mySql = require("mysql");
+const mysql = require("mysql");
+
+var con = mysql.createConnection({ host: "dmitribilyk.com", user: "dmitribi_TTT", password: "kar139", database: "dmitribi_nodeTicTacToe" });
+con.connect((err) => {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log("successfully connected to database!")
+  }
+
+  con.query("SELECT * FROM users", (err, result, fields) => {
+    console.log(err + "\n" + JSON.stringify(result) + "\n" + JSON.stringify(fields));
+
+
+  })
+  //con.query("INSERT INTO users (username,score) VALUES ('ronburgundy','1000000');");
+  //con.query("DELETE FROM users WHERE username='ronburgundy'");
+
+})
