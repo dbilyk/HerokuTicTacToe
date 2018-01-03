06f91542d47d4cabc9bd20af6c5bb28f8e29acd6
Marklar <dmitri.bilyk@gmail.com>
2018-01-03 00:25:40 -0800

    one more time

---
+++ public/javascript/game.js
@@ -0,0 +1,194 @@
+function GameRoutes(io, lobView) {
+  var socket = io
+  var lobbyView = lobView
+  var view = new GameView()
+  var myTurn = false
+  var animSpeed = 1.5
+  var roomID
+
+  //user send input
+  this.userInput = function (canvas) {
+    if (view.getState() == null && !$(canvas).prop("checked") && myTurn) {
+      let id = $(canvas)[0].id
+      var position = id.match(/\d/)[0]
+      socket.emit("playTurn", { roomID: roomID, position: position })
+      $(canvas).prop("checked", true)
+      view.animate("X", canvas, animSpeed)
+      lobbyView.bounce($("#opponent-name"))
+      myTurn = false;
+    }
+  }
+
+  socket.on("gameSetup", (object) => {
+    //choose who gets to go first
+    myTurn = object.opponentStatus.myTurn
+    roomID = object.roomID
+    view.setup()
+  })
+
+  //recieve a turn
+  socket.on("playTurn", (pos) => {
+    $($("canvas")[pos]).prop("checked", true)
+    view.animate('O', $("canvas")[pos], animSpeed)
+
+    myTurn = true;
+  })
+  //server confirmed win!
+  socket.on("youWon", (winningPos) => {
+    view.animate("X", $("canvas")[winningPos], animSpeed)
+    lobbyView.updateHeader("you WON!")
+    view.exitGame(true)
+  })
+  //server confirmed loss...
+  socket.on("youLost", (winningPos) => {
+    lobbyView.updateHeader("you LOST!")
+    view.animate("O", $("canvas")[winningPos], animSpeed)
+    view.exitGame(false)
+  })
+
+  socket.on("draw", () => {
+    lobbyView.updateHeader("draw...")
+    view.exitGame()
+  })
+
+
+}
+
+function GameView() {
+  var animStart
+  var currentAnimFrame = null
+  var firstLineSaved = false
+  this.getState = () => currentAnimFrame;
+  this.animate = function (isX, canvas, speed) {
+    animStart = Date.now()
+
+    var requestAnimationFrame =
+      window.requestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.msRequestAnimationFrame;
+
+    //if were not animating
+    if (currentAnimFrame == null) {
+      //drawing X or O?
+      if (isX.toLowerCase() == "x")
+        currentAnimFrame = requestAnimationFrame(() => this.drawX(canvas, speed))
+      else
+        currentAnimFrame = requestAnimationFrame(() => this.drawO(canvas, speed))
+    }
+  }
+  this.setup = function () {
+    $("canvas").prop("checked", false)
+    for(var i =0; i< 9;i++){
+      var canvas = $("canvas")[i]
+      let ctx = canvas.getContext("2d")
+      ctx.clearRect(0,0,$("canvas").outerWidth(),$("canvas").outerHeight())
+      $("#left-col").css("opacity","0")
+    }
+  }
+
+  this.drawX = function (canvas, speed) {
+    var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+    var time = (new Date().getTime() - animStart) * 0.002 * speed;
+
+    var width = (canvas.width)
+    var height = (canvas.height)
+    var ctx = canvas.getContext('2d')
+    var pad = 10
+    ctx.lineWidth = 4 * time
+    ctx.strokeStyle = "#62db5c"
+    //drawing first line
+    if (width - pad > (width - pad) * time && !firstLineSaved) {
+      ctx.beginPath()
+      ctx.moveTo(0 + pad, 0 + pad)
+      ctx.lineTo((width - pad) * time, (height - pad) * time)
+      ctx.stroke()
+      ctx.closePath()
+      currentAnimFrame = requestAnimationFrame(() => this.drawX(canvas, speed))
+    }
+    //drawing second line AFTER the first line
+    else {
+      if (!firstLineSaved) {
+        firstLineSaved = true;
+        animStart = Date.now()
+        time = (new Date().getTime() - animStart) * 0.002 * speed;
+        ctx.save()
+      }
+      if (0 + pad < (width - pad) - ((width - pad) * time)) {
+        ctx.restore()
+        ctx.beginPath()
+        ctx.moveTo(width - pad, pad)
+        ctx.lineTo((width - pad) - ((width - pad) * time), (height - pad) * time + pad)
+        ctx.stroke()
+
+        currentAnimFrame = requestAnimationFrame(() => this.drawX(canvas, speed))
+
+      }
+      //done drawing stuff
+      else {
+        currentAnimFrame = null;
+        firstLineSaved = false;
+      }
+    }
+
+
+  }
+  this.drawO = function (canvas, speed) {
+    var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+    var time = (new Date().getTime() - animStart) * 0.002 * speed;
+    var ctx = canvas.getContext("2d")
+    ctx.strokeStyle = "#db685c"
+    ctx.lineWidth = 4 * time
+    ctx.moveTo(canvas.width, canvas.height / 2)
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height / 2, ((canvas.width / 2) + (canvas.height / 2)) / 2.4, 0 * Math.PI, 2 * Math.PI * time)
+    ctx.stroke()
+    if (2 * time < 2) {
+      currentAnimFrame = requestAnimationFrame(() => this.drawO(canvas, speed))
+
+    }
+    else {
+      currentAnimFrame = null
+    }
+
+  }
+
+  function exit(colorClass) {
+    $("#gameboard").addClass(colorClass);
+    $("#opponent-name, #opponent-name>h3").animate({maxWidth:0},500)
+    $("#gameboard").animate({ backgroundColor: "white", opacity: 0, height: "0px" }, 2000, () => {
+      $("#gameboard").hide()
+      $("#opponent-name").hide('fast').css({maxWidth:"50%",marginLeft:"0.5em"})
+      $("#users-online").show("fast")
+      $(".user-div").show()
+      $(".incoming-notification").css('opacity','1')
+      $("#play-btn").show("fast")
+      $("#gameboard").removeClass(colorClass).css({ opacity: 1, height: 'auto' })
+      
+    })
+  }
+  this.exitGame = function (wonBool) {
+    if (wonBool && arguments.length > 0) {
+      exit("winColor")
+    }
+    else if (!wonBool && arguments.length > 0) {
+      exit("loseColor")
+    }
+    else {
+      exit("drawColor")
+    }
+  }
+
+
+
+}
+
+function ClientGameEvents(io, lobbyView) {
+
+  var gameRoutes = new GameRoutes(io, lobbyView);
+  $("#gameboard").on('click', "canvas", (e) => {
+    var canvas = $(e.target)[0];
+    gameRoutes.userInput(canvas)
+  })
+}
+
---
+++ roomRoutes.js
@@ -0,0 +1,297 @@
+const express = require("express")
+const database = require("./models")
+const db = new database();
+const hash = require("hashids")
+var HASH = new hash()
+function challenge(target, requester) {
+  this.target = target;
+  this.requester = requester;
+
+}
+
+function User(username, socket) {
+  this.username = username;
+  this.socket = socket;
+}
+
+function turnData(roomID, pos) {
+  this.roomID = roomID;
+  this.opponentStatus = pos;
+
+}
+//array of {username:"somename",id:"socketid"}
+var currentUsers = [];
+var usernameList = () => currentUsers.map(e => e.username)
+// [{requester:socketID, target: socketID}]
+var activeChallenges = []
+var activeGames = []
+
+function Game(socketA, socketB, io) {
+  var IO = io;
+  var idSet = false;
+  var socketA = socketA
+  var socketB = socketB
+  var totalTurns = 0;
+
+  this.roomID;
+
+  this.p1 = { positions: [0, 0, 0, 0, 0, 0, 0, 0, 0], myTurn: true }
+  this.p2 = { positions: [0, 0, 0, 0, 0, 0, 0, 0, 0], myTurn: false }
+
+  function checkForWin(array) {
+    winCases =
+      [[1, 0, 0, 0, 1, 0, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 1, 0, 0],
+      [0, 1, 0, 0, 1, 0, 0, 1, 0],
+      [0, 0, 1, 0, 0, 1, 0, 0, 1],
+      [1, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 1, 1],
+      [0, 0, 1, 0, 1, 0, 1, 0, 0]]
+
+    var winCount = 0;
+    var count = 0;
+    var won = false;
+    winCases.forEach((_, i) => {
+      winCases[i].forEach((__, j) => {
+        if (__ == 1 && array[j] == 1) {
+          winCount++;
+
+          if (winCount == 3) won = true;
+
+        }
+        count++;
+        if (count > 8) {
+          count = 0;
+          winCount = 0;
+        }
+      })
+    })
+    return won;
+  }
+  //join sockets to room
+  function roomIDSetCallback() {
+    socketA.join(this.roomID)
+    socketB.join(this.roomID)
+  }
+  //called once to push blank state to clients
+  this.setUpGame = function () {
+    if (this.roomID) {
+      var initDataA = new turnData(this.roomID, this.p1)
+      var initDataB = new turnData(this.roomID, this.p2)
+      io.to(socketA.id).emit("gameSetup", initDataA)
+      io.to(socketB.id).emit("gameSetup", initDataB)
+    }
+  }
+
+  //set encoded id based on array index for easy lookup
+  this.setRoomID = (arrayLoc) => {
+    if (!idSet) {
+      this.roomID = HASH.encode(arrayLoc);
+      roomIDSetCallback()
+      idSet = true;
+    }
+  }
+
+  this.newInput = function (socket, pos) {
+    var targetData, target, callerData, caller
+    if (socket.id == socketA.id) {
+      targetData = this.p2;
+      target = socketB;
+      callerData = this.p1;
+      caller = socketA;
+    }
+    else {
+      targetData = this.p1
+      target = socketA;
+      callerData = this.p2;
+      caller = socketB;
+    }
+    //if its this socket's turn
+    if (callerData.myTurn) {
+      if (targetData.positions[pos] == 0) {
+        callerData.positions[pos] = 1;
+        //check for win
+        if (checkForWin(callerData.positions) && totalTurns <= 9) {
+
+          io.to(caller.id).emit("youWon", pos)
+          io.to(caller.id).emit("updateLobby", usernameList());
+          io.to(target.id).emit("youLost", pos)
+          io.to(target.id).emit("updateLobby", usernameList());
+        }
+        else {
+          socket.to(target.id).emit("playTurn", pos)
+          callerData.myTurn = false;
+          targetData.myTurn = true;
+          totalTurns++
+          console.log(totalTurns)
+          if (totalTurns == 9) {
+            io.to(caller.id).emit("draw")
+            io.to(caller.id).emit("updateLobby", usernameList());
+            io.to(target.id).emit("draw")
+            io.to(target.id).emit("updateLobby", usernameList());
+
+          }
+        }
+
+      }
+    }
+
+  }
+  this.opponentLeft = function (socket) {
+
+  }
+
+}
+
+
+module.exports = function (io) {
+  router = express.Router()
+
+  //send the home rooms view to client
+  router.get("/", (req, res) => {
+    res.render("./rooms.pug")
+  })
+
+
+
+  function findSocketFromUsername(username) {
+    var targetUserData = currentUsers.find(e => e.username == username)
+    return (targetUserData) ? targetUserData.socket : false;
+  }
+
+  function findUsernameFromSocket(socket) {
+    var targetUserData = currentUsers.find(e => e.socket.id == socket.id)
+
+    return (targetUserData) ? targetUserData.username : false;
+  }
+
+  function findChallenge(userSocketID) {
+    var index = activeChallenges.findIndex((e) => {
+      return (e.target.id == userSocketID || e.requester.id == userSocketID)
+    })
+    return { index: index, object: activeChallenges[index] }
+  }
+
+  function findEmptyIndex(gameArr) {
+    var i = gameArr.findIndex((e) => undefined);
+    return (i == -1) ? gameArr.length : i
+  }
+  //socket io connection
+  io.on('connection', (socket) => {
+
+    //send a welcome event
+    socket.emit("welcome", "connected through the roomRoutes File!")
+
+    socket.on("updateLobby", () => {
+      socket.to(socket.id).emit("updateLobby", usernameList())
+    })
+
+    socket.emit("updateLobby", usernameList());
+
+    //client request login
+    socket.on('loginRequest', (credentials) => {
+      db.userAuth(credentials, loggedIn);
+
+    })
+
+    //login result
+    function loggedIn(bool, user) {
+      //check if user has already logged in elsewhere.
+      if (currentUsers.findIndex(e => e.username == user) == -1) {
+        socket.emit('loginResult', bool, user)
+        if (bool) {
+          currentUsers.push(new User(user, socket))
+
+          io.sockets.emit("updateLobby", usernameList());
+        }
+      }
+    }
+
+    //client request signup
+    socket.on('signupRequest', (data) => {
+      //callback only fired on success with a bool arg
+      db.insertNewUser(data, signedUp)
+    })
+
+    //signup result
+    function signedUp(bool) {
+      socket.emit('signupResult', bool);
+    }
+
+    //someone challenged someone else
+    socket.on('newChallenge', (targetUser, callingUser) => {
+      let targetSocket = findSocketFromUsername(targetUser)
+      let callingSocket = findSocketFromUsername(callingUser)
+      if (targetSocket) {
+        let newChallenge = new challenge(targetSocket, callingSocket)
+        activeChallenges.push(newChallenge)
+        socket.to(targetSocket.id).emit("newChallenge", callingUser)
+
+      }
+    })
+
+
+
+
+    //pass answer to relevant socket
+    socket.on("challengeResponse", (accepted) => {
+
+      let thisChallenge = findChallenge(socket.id)
+      socket.to(thisChallenge.object.requester.id).emit("challengeResponse", accepted)
+
+      if (accepted) {
+        var newGame = new Game(socket, thisChallenge.object.requester, io)
+        var emptyIndex = findEmptyIndex(activeGames)
+        activeGames.splice(emptyIndex, 0, newGame)
+        newGame.setRoomID(emptyIndex)
+        newGame.setUpGame()
+        activeChallenges.splice(thisChallenge.index, 1)
+
+
+      }
+
+
+    })
+
+    socket.on("cancelChallenge", () => {
+      var challenge = findChallenge(socket.id);
+      var requesterUsername = findUsernameFromSocket(challenge.object.requester.id)
+      if (requesterUsername) {
+        socket.to(challenge.object.target.id).emit("cancelChallenge", requesterUsername)
+
+      }
+      io.emit("updateLobby", usernameList())
+
+      activeChallenges.splice(challenge.index, 1)
+    })
+
+
+
+    //some player hit X
+    socket.on("playTurn", (object) => {
+      var index = HASH.decode(object.roomID)[0];
+      var targetGame = activeGames[index]
+      targetGame.newInput(socket, object.position)
+
+
+    })
+
+
+    //disconnect
+    socket.on("disconnect", (data) => {
+      let indexToRemove = currentUsers.findIndex(e => e.socket.id == socket.id)
+
+      if (indexToRemove != -1) currentUsers.splice(indexToRemove, 1);
+      io.sockets.emit("updateLobby", usernameList())
+    })
+
+
+
+  })
+
+
+
+
+  return router;
+}
